ed440ccba30cf00e41dae46c8bf3937b142fdcdd
NewGardenGloucester <86858303+NewGardenGloucester@users.noreply.github.com>
2021-07-19 22:35:43 +0100

    Create js.js

---
+++ menu/javascript/js.js
@@ -0,0 +1,99 @@
+// scroll on navbar click
+$('.navbar a').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body')
+      .animate({
+        scrollTop: $(hash).offset().top
+      },800);
+  }
+});
+
+// typed.js
+
+const options = {
+  strings: ['New Garden ^1000', ' from Gloucester ^1000', ' a Cantonese and Chinese takeaway ^1000'],
+  typeSpeed: 100,
+  backSpeed: 75,
+  loop: true,
+};
+
+const typed = new Typed('#type', options);
+
+
+// form
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(el => {
+  el.addEventListener('blur', e => {
+    if(e.target.value) {
+      e.target.classList.add('dirty');
+    } else {
+      e.target.classList.remove('dirty');
+    }
+  })
+})
+
+
+// scroll show shadow
+const debounce = (fn) => {
+
+
+  let frame;
+
+  return (...params) => {
+
+
+    if (frame) {
+      cancelAnimationFrame(frame);
+    }
+
+
+    frame = requestAnimationFrame(() => {
+
+
+      fn(...params);
+    });
+
+  }
+};
+
+const storeScroll = () => {
+  document.documentElement.dataset.scroll = window.scrollY;
+}
+document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+storeScroll();
+
+
+
+
+
+ // pageclip
+var form = document.querySelector('.pageclip-form')
+Pageclip.form(form, {
+  onSubmit: function (event) { show_alert() },
+  onResponse: function (error, response) { return false },
+  successTemplate: '<span>Thank you!</span>'
+})
+
+// alerts
+function show_alert() {
+  var x = document.getElementById("alert");
+  x.style.display = "block";
+  x.style.opacity = "1";
+}
+
+var close = document.getElementsByClassName("closebtn");
+var i;
+
+
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function(){
+    var div = this.parentElement;
+    div.style.opacity = "0";
+    setTimeout(function(){ div.style.display = "none"; }, 600);
+  }
+}
